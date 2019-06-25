f150ac02d7bfa2155555c8d7aa06e8de7f201750
Rajat Murthi <Rajat.Murthi@accolade.com>
2019-06-24 17:53:07 -0700

    Get simple state working with global hooks. Using reactn, use hook pattern with globals (which use new react hooks internally).

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+import { setGlobal } from 'reactn'
+
+export const onClientEntry = () => {
+    setGlobal({
+        name: "Default Name",
+        species: {
+            name: "Human",
+            brawn: 1,
+            agility: 1,
+            intellect: 1,
+            wisdom: 1,
+            force: 0
+        },
+        carrer: {
+            name: "smuggler",
+            speciality: "Gunslinger"
+        }
+
+    })
+}
